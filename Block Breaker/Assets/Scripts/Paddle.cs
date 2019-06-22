de6e71e6491a360241a9cc4cb7fb19e054698332
LordSayur <mohammad.omar.317@gmail.com>
2019-06-22 14:07:38 +0800

    Limit With Mathf.Clamp()

--- Block Breaker/Assets/Scripts/Paddle.cs
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -6,18 +6,27 @@ public class Paddle : MonoBehaviour
 {
     [SerializeField]
     private float screenWidthInUnits = 16f;
+    [SerializeField]
+    private float paddleWidth = 2f;
+
+    private float paddlePosMin = 0f;
+    private float paddlePosMax;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        paddlePosMax = screenWidthInUnits;
+        paddlePosMin += paddleWidth / 2f;
+        paddlePosMax -= paddleWidth / 2f;
     }
 
     // Update is called once per frame
     void Update()
     {
         float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePos = new Vector2(mousePositionInUnits, transform.position.y);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePositionInUnits, paddlePosMin, paddlePosMax);
         transform.position = paddlePos;
     }
 }
